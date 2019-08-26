04bc340e3d9575c0200b1e2286b3aeb61eea8ab7
rongfengliang <1141591465@qq.com>
2019-08-26 14:26:25 +0800

    add more demo code

--- podlets/userlogin/index.js
+++ podlets/userlogin/index.js
@@ -15,6 +15,9 @@ app.use(podlet.middleware());
 app.get('/api', (req, res) => {
     res.json([{ name: 'fluffy' }]);
 });
+
+app.use('/assets', express.static('assets'));
+
 podlet.js({ value: 'https://passport.baidu.com/static/passpc-base/js/dv/g.min.js' });
 podlet.css({ value: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css' });
 
@@ -22,6 +25,7 @@ app.get(podlet.content(), (req, res) => {
     res.status(200).podiumSend(`
         <div>
             This is the  userlogin podlet's HTML content
+            <img src="/assets/page.png"  />
         </div>
     `);
 });
